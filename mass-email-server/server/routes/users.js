575b998601819cfb4f1db7363b3a0dec58db4e29
coreyjjames <cjames29@myseneca.ca>
2020-04-07 12:25:29 -0400

    setup

---
+++ mass-email-server/server/routes/users.js
@@ -0,0 +1,9 @@
+var express = require('./node_modules/express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
